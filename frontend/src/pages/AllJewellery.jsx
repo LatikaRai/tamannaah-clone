4404bb306d232166e0d0b7cd5f8ba00b45892a99
f <lr5370566@gmail.com>
2026-07-06 23:08:58 +0530

    created a reusable layout for products listings

--- frontend/src/pages/AllJewellery.jsx
+++ frontend/src/pages/AllJewellery.jsx
@@ -1,39 +1,12 @@
-import { useState } from 'react'
-import ProductCard from '../components/ui/ProductCard'
 import { useSelector } from 'react-redux'
+import ProductsListingLayout from '../components/ui/ProductsListingLayout'
 
 const AllJewellery = () => {
   const {products} = useSelector(state => state.productReducer)
     const allProducts = [...products]
 
-    const [viewImages, setViewImages] = useState('four')
   return (
-    <div className='w-full px-[2.5em]'>
-        <div className='w-full h-[30vh]'></div>
-        <div className='w-full flex items-center justify-between pb-[2.5em] text-[0.9rem]'>
-            <div className='flex items-center gap-2'>
-                <h3>View:</h3>
-                <h3 
-                onClick={()=>{
-                    setViewImages('four')
-                }}
-                className={`${viewImages === 'four' ? 'text-black underline underline-offset-2' : 'text-gray-400'} cursor-pointer`}
-                >Four</h3>
-                <h3
-                onClick={()=>{
-                    setViewImages('two')
-                }}
-                className={`${viewImages === 'two' ? 'text-black underline underline-offset-2' : 'text-gray-400'} cursor-pointer`}
-                >Two</h3>
-            </div>
-            <div>Filter & Sort</div>
-        </div>
-        <div className='w-full h-auto flex flex-wrap items-center gap-[2.2em]'>
-      {allProducts.map(product => {
-        return <ProductCard key={product.slug} product={product} viewImages={viewImages}/>
-      })}
-    </div>
-    </div>
+    <ProductsListingLayout products={allProducts}/>
   )
 }
 
